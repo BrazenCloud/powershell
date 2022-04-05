7f39ef6b619a750edbc546618842835055a19c8e
Anthony Howell <anthony@howell-it.com>
2022-04-05 14:34:53 -0700

    update connect-runway to support alternate domains

--- custom/Connect-Runway.ps1
+++ custom/Connect-Runway.ps1
@@ -5,8 +5,11 @@ Function Connect-Runway {
         [Parameter(Mandatory)]
         [string]$Email,
         [Parameter(Mandatory)]
-        [SecureString]$Password
+        [SecureString]$Password,
+        [string]$RunwayDomain = 'portal.runway.host'
     )
+    $env:RunwayDomain = $RunwayDomain
+
     $s = Invoke-RwLoginAuthentication -Email $Email -Password ([pscredential]::new('blah',$Password).GetNetworkCredential().Password) -Remember
     $env:RunwaySessionToken = $s.Session
 }
