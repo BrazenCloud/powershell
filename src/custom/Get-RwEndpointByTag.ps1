aaa0c9bb8c33510cb71224b8cc0b33fa3dd5185e
Anthony Howell <anthony@howell-it.com>
2022-04-13 14:33:27 -0700

    adding get-rwendpointbytag

---
+++ src/custom/Get-RwEndpointByTag.ps1
@@ -0,0 +1,39 @@
+Function Get-RwEndpointByTag {
+    [CmdletBinding()]
+    param (
+        [string[]]$Tags
+    )
+
+    if ($tags.Count -gt 1) {
+        $filterChildren = foreach ($tag in $tags) {
+            @{
+                Left = 'Tags'
+                Operator = '='
+                Right = $tag
+            }
+        }
+        $query = @{
+            includeSubgroups = $true
+            skip = 0
+            take = 100
+            sortDirection = 0
+            filter = @{
+                children = $filterChildren
+                operator = 'AND'
+            }
+        }
+    } else {
+        $query = @{
+            includeSubgroups = $true
+            skip = 0
+            take = 100
+            sortDirection = 0
+            filter = @{
+                Left = 'Tags'
+                Operator = '='
+                Right = $Tags[0]
+            }
+        }
+    }
+    (Invoke-RwQueryEndpointAsset -Query $query).Items
+}
