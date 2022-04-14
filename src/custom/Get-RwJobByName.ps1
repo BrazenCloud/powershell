89c01886adb56097aca4caf96d21e58011028b1c
Anthony Howell <anthony@howell-it.com>
2022-04-14 08:40:52 -0700

    supports multiple job names

--- src/custom/Get-RwJobByName.ps1
+++ src/custom/Get-RwJobByName.ps1
@@ -2,17 +2,37 @@ Function Get-RwJobByName {
     [cmdletbinding()]
     param (
         [Alias('Name')]
-        [string]$JobName
+        [string[]]$JobName
     )
-    $query = @{
-        includeSubgroups = $true
-        skip = 0
-        take = 100
-        SortDirection = 0
-        filter = @{
-            Left = 'Name'
-            Operator = '='
-            Right = $JobName
+    if ($JobName.Count -gt 1) {
+        $filterChildren = foreach ($name in $JobName) {
+            @{
+                Left = 'Name'
+                Operator = '='
+                Right = $name
+            }
+        }
+        $query = @{
+            includeSubgroups = $true
+            skip = 0
+            take = 100
+            sortDirection = 0
+            filter = @{
+                children = $filterChildren
+                operator = 'OR'
+            }
+        }
+    } else {
+        $query = @{
+            includeSubgroups = $true
+            skip = 0
+            take = 100
+            SortDirection = 0
+            filter = @{
+                Left = 'Name'
+                Operator = '='
+                Right = $JobName[0]
+            }
         }
     }
     (Invoke-RwQueryJob -Query $query).Items
