48b326fb4d4110501329375f73e56db22eb6f605
Anthony Howell <anthony@howell-it.com>
2022-04-14 08:39:07 -0700

    supports multiple names now

--- src/custom/Get-RwConnectionByName.ps1
+++ src/custom/Get-RwConnectionByName.ps1
@@ -2,17 +2,37 @@ Function Get-RwConnectionByName {
     [CmdletBinding()]
     param (
         [Alias('Name')]
-        [string]$ConnectionName
+        [string[]]$ConnectionName
     )
-    $query = @{
-        includeSubgroups = $true
-        skip = 0
-        take = 100
-        SortDirection = 0
-        filter = @{
-            Left = 'Name'
-            Operator = '='
-            Right = $ConnectionName
+    if ($ConnectionName.Count -gt 1) {
+        $filterChildren = foreach ($name in $ConnectionName) {
+            @{
+                Left = 'Name'
+                Operator = '='
+                Right = $name
+            }
+        }
+        $query = @{
+            includeSubgroups = $true
+            skip = 0
+            take = 100
+            sortDirection = 0
+            filter = @{
+                children = $filterChildren
+                operator = 'OR'
+            }
+        }
+    } else {
+        $query = @{
+            includeSubgroups = $true
+            skip = 0
+            take = 100
+            SortDirection = 0
+            filter = @{
+                Left = 'Name'
+                Operator = '='
+                Right = $ConnectionName
+            }
         }
     }
     (Invoke-RwQueryConnection -Query $query).Items
