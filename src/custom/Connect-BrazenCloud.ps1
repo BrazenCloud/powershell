d9439e9a32078f2d7ffd04cc479bb52f1d3cf216
Anthony Howell <anthony@howell-it.com>
2022-09-29 15:07:27 -0700

    fixing exec auth

--- src/custom/Connect-BrazenCloud.ps1
+++ src/custom/Connect-BrazenCloud.ps1
@@ -17,10 +17,18 @@ Function Connect-BrazenCloud {
 
     Set-BcDefaultGroup -GroupId (Get-BcAuthenticationCurrentUser).HomeContainerId
 
-    if (-not ($SkipExecutableDownload.IsPresent) -and -not (Test-Path $PSScriptRoot\runway)) {
-        Get-BcExecutable -OutFile $PSScriptRoot/runway
-        if (Test-Path $PSScriptRoot\runway) {
-            & $PSScriptRoot\runway -N -S $Domain login -u $Email -p ([pscredential]::new('blah', $Password).GetNetworkCredential().Password)
-        }
+    $fileName = if ($PSVersionTable.PSVersion -lt [version]'6.0.0' -or $IsWindows) {
+        'runway.exe'
+    } else {
+        'runway'
+    }
+
+    $env:BrazenCloudUtilityPath = "$PSScriptRoot\$fileName"
+
+    if (-not ($SkipExecutableDownload.IsPresent) -and -not (Test-Path $env:BrazenCloudUtilityPath)) {
+        Get-BcExecutable -OutFile $env:BrazenCloudUtilityPath
+    }
+    if (Test-Path $env:BrazenCloudUtilityPath) {
+        & $env:BrazenCloudUtilityPath -N -S $Domain login -u $Email -p ([pscredential]::new('blah', $Password).GetNetworkCredential().Password) | Out-Null
     }
 }
