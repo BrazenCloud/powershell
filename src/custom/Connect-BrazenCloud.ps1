c22d95ffc145647dc14ce0258e58a013105768fd
Anthony Howell <anthony@howell-it.com>
2022-09-29 14:16:47 -0700

    adding executable retrieval

--- src/custom/Connect-BrazenCloud.ps1
+++ src/custom/Connect-BrazenCloud.ps1
@@ -6,12 +6,21 @@ Function Connect-BrazenCloud {
         [string]$Email,
         [Parameter(Mandatory)]
         [SecureString]$Password,
-        [string]$Domain = 'portal.brazencloud.com'
+        [string]$Domain = 'portal.brazencloud.com',
+        [int]$TTL = 20,
+        [switch]$SkipExecutableDownload
     )
     $env:BrazenCloudDomain = $Domain
 
-    $s = Invoke-BcLoginAuthentication -Email $Email -Password ([pscredential]::new('blah', $Password).GetNetworkCredential().Password) -Remember
+    $s = Invoke-BcLoginAuthentication -Email $Email -Password ([pscredential]::new('blah', $Password).GetNetworkCredential().Password) -Remember -Ttl $TTL
     $env:BrazenCloudSessionToken = $s.Session
 
     Set-BcDefaultGroup -GroupId (Get-BcAuthenticationCurrentUser).HomeContainerId
+
+    if (-not ($SkipExecutableDownload.IsPresent) -and -not (Test-Path $PSScriptRoot\runway)) {
+        Get-BcExecutable -OutFile $PSScriptRoot/runway
+        if (Test-Path $PSScriptRoot\runway) {
+            & $PSScriptRoot\runway -N -S $Domain login -u $Email -p ([pscredential]::new('blah', $Password).GetNetworkCredential().Password)
+        }
+    }
 }
---
+++ src/custom/Get-BcExecutable.ps1
@@ -0,0 +1,45 @@
+Function Get-BcExecutable {
+    [cmdletbinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$OutFile
+    )
+
+    $platform = if ($PSVersionTable.PSVersion -lt [version]'6.0.0' -or $IsWindows) {
+        if (Test-Path 'C:\Program Files (x86)') {
+            'windows64'
+        } else {
+            'windows32'
+        }
+    } elseif ($IsLinux) {
+        $idLike = (Get-Content /etc/*-release | ConvertFrom-StringData).ID_LIKE -split ' '
+        :idlike foreach ($il in $idLike) {
+            switch -Regex ($il) {
+                'rhel|fedora' {
+                    Write-Warning 'RHEL and Fedora are not currently supported for streaming. All other SDK functions will still work.'
+                    break :idlike
+                }
+                'debian' {
+                    if (arch -eq 'x86_64') {
+                        'debian64'
+                    } else {
+                        'debian32'
+                    }
+                }
+                default {
+                    Write-Warning 'Your OS may not be supported for the streaming feature. Downloading the general linux executable...'
+                    if (arch -eq 'x86_64') {
+                        'linux64'
+                    } else {
+                        'linux32'
+                    }
+                }
+            }
+        }
+    }
+
+    if ($platform -ne $null) {
+        Write-Verbose "Downloading the '$platform' binary..."
+        Invoke-BcDownloadContentPublicFile -Platform $Platform -Key 'runway' -OutFile $OutFile
+    }
+}
